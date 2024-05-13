cbc1980c4a0d5165a47a3e5dbca0b4a55e75e483
krupalilane <kpatel@lane-enterprises.com>
2024-05-13 08:02:24 -0400

    view page

--- application/controllers/Manage_project.php
+++ application/controllers/Manage_project.php
@@ -34,6 +34,7 @@ class Manage_project extends MY_Controller {
 	}
 	public function view($id)
 	{
+		$project_id = 'Project #P'.str_pad($id, 5, '0', STR_PAD_LEFT);
 		$breadcrumb_data = array(
 			'0' => array(
 				'url'	=> site_url('manage_project'),
@@ -45,29 +46,49 @@ class Manage_project extends MY_Controller {
 			),
 			'2' => array(
 				'url'	=> '',
-				'name'	=> 'Project #P'.str_pad($id, 5, '0', STR_PAD_LEFT)
+				'name'	=> $project_id
 			) 
 		);
 		$this->data['breadcrumb'] = $breadcrumb_data;
 		// Execute the stored procedure using SQLSRV driver
-		$stmt = sqlsrv_query($this->db->conn_id, 'EXEC stormconfig.USP_GetQuoteProductList @QuoteId = 3');
+		$stmt = sqlsrv_query($this->db->conn_id, 'EXEC stormconfig.USP_GetQuoteProductList @QuoteId = '.$id);
 		if (!$stmt) {
 		    return false;
 		}
 
-		$resultSets = array();
+		$quote_resultSets = array();
 		do {
 		    $resultSet = array();
 		    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
 		        $resultSet[] = $row;
 		    }
-		    $resultSets[] = $resultSet;
+		    $quote_resultSets[] = $resultSet;
 		} while (sqlsrv_next_result($stmt));
 
 		sqlsrv_free_stmt($stmt);
-		  echo "<pre>";
-		  print_r($resultSets);
-		  exit;
-
+		// echo "<pre>";
+		// print_r($quote_resultSets);
+		// exit;
+		
+		$quote_data 		= array();
+		$product_data 		= array();
+		$product_file_data	= array();
+		if (!empty($quote_resultSets)) {
+			if (isset($quote_resultSets[0][0])) {
+				$quote_data = $quote_resultSets[0][0];
+			}
+			if (isset($quote_resultSets[1][0])) {
+				$product_data = $quote_resultSets[1][0];
+			}
+			if (isset($quote_resultSets[2])) {
+				$product_file_data = $quote_resultSets[2];
+			}
+		}
+		$this->data['quote_data'] 			= $quote_data;
+		$this->data['product_data'] 		= $product_data;
+		$this->data['product_file_data']	= $product_file_data;
+		echo "<pre>";
+		print_r($this->data);
+		exit;
 	}
 }
---
+++ application/views/manage_project/view.php
@@ -0,0 +1,168 @@
+<!-- BEGIN PAGE CONTENT INNER -->
+<div class="page-content-inner">
+    <div class="row">
+        <div class="col-md-12">
+            <!-- Begin: life time stats -->
+            <div class="portlet light portlet-fit portlet-datatable ">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-settings font-dark"></i>
+                        <span class="caption-subject font-dark sbold uppercase"> Project #P001229 | testing cmp </span>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <div class="tabbable-line">
+                        <ul class="nav nav-tabs nav-tabs-lg">
+                            <li class="active">
+                                <a href="#tab_1" data-toggle="tab"> Details </a>
+                            </li>
+                            <li>
+                                <a href="#tab_2" data-toggle="tab"> Attached Files
+                                    <span class="badge badge-danger">5</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="tab_1">
+                                <div class="row">
+                                    <div class="col-md-12 col-sm-12">
+                                        <div class="portlet red box">
+                                            <div class="portlet-title">
+                                                <div class="caption"><i class="fa fa-cubes"></i>Project Details </div>
+                                            </div>
+                                            <div class="portlet-body">
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Project #: </div>
+                                                    <div class="col-md-7 value"> P001229 </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Project Name: </div>
+                                                    <div class="col-md-7 value"> testing cmp </div>
+                                                </div>                                                         
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Project Created: </div>
+                                                    <div class="col-md-7 value"> May 2, 2024 6:06PM </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Project Status: </div>
+                                                    <div class="col-md-7 value"><i class="fa fa-check-square-o"></i>&nbsp;Build Complete 
+                                                    </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Product: </div>
+                                                    <div class="col-md-7 value"> StormKeeper&trade; </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12 col-sm-12">
+                                        <div class="portlet red box">
+                                            <div class="portlet-title">
+                                                <div class="caption">
+                                                    <i class="fa fa-cube"></i>Product Details </div>
+                                            </div>
+                                            <div class="portlet-body">
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Volume Needed (cf): </div>
+                                                    <div class="col-md-7 value"> 48000  </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Soil Bearing (ksf): </div>
+                                                    <div class="col-md-7 value">  </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Maximum Burial Depth (ft): </div>
+                                                    <div class="col-md-7 value"> 10.00 </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Length (ft): </div>
+                                                    <div class="col-md-7 value"> 200.00 </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Width (ft): </div>
+                                                    <div class="col-md-7 value"> 100.00 </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Manifold Chamber Size: </div>
+                                                    <div class="col-md-7 value">  </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Manifold: </div>
+                                                    <div class="col-md-7 value">  </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Manifold Header Size: </div>
+                                                    <div class="col-md-7 value">  </div>
+                                                </div>
+                                                <div class="row static-info">
+                                                    <div class="col-md-5 name"> Manifold Stub Size: </div>
+                                                    <div class="col-md-7 value">  </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12 col-sm-12">
+                                        <a class="btn red" href="<?php echo site_url('manage_project');?>">Back To Projects</a>
+                                    </div>
+                                </div>                                                                   
+                            </div>
+                            <div class="tab-pane" id="tab_2">
+                                <div class="row">
+                                    <div class="col-md-12 col-sm-12">                                                                    
+                                        <div class="portlet box red">
+                                            <div class="portlet-title">
+                                                <div class="caption"><i class="fa fa-file-o"></i>Attached Files </div>
+                                            </div>
+                                            <div class="portlet-body">
+                                                <div class="table-responsive">
+                                                    <table class="table">
+                                                        <thead>
+                                                            <tr>
+                                                                <th> File Name </th>
+                                                                <th> File Type </th>
+                                                                <th> File Size </th>
+                                                                <th> File Created </th>
+                                                                <th> Actions </th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>Pipe Title Page dwg.dwg</td>
+                                                                <td><i class='fa fa-file-o'></i>&nbsp DWG Drawing </td>
+                                                                <td>17.21kB</td>
+                                                                <td>2024-05-02 18:06:54</td>
+                                                                <td> <a class="btn btn-xs red" target="_blank" href="download_file.php?id=6557">Download File</a> </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Pipe Title Page dwg.dwg</td>
+                                                                <td><i class='fa fa-file-o'></i>&nbsp DWG Drawing </td>
+                                                                <td>17.21kB</td>
+                                                                <td>2024-05-02 18:06:54</td>
+                                                                <td> <a class="btn btn-xs red" target="_blank" href="download_file.php?id=6557">Download File</a> </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12 col-sm-12">
+                                        <a class="btn red" href="manage_projects.php">Back To Projects</a>
+                                    </div>
+                                </div>                                                                       
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End: life time stats -->
+        </div>
+    </div>
+</div>
+<!-- END PAGE CONTENT INNER -->                                    
